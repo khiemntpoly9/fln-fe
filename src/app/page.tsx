5b1d1fb6e3841fe1fd0e726120c7ca309220df11
manden520 <92621988+manden520@users.noreply.github.com>
2023-07-26 14:52:54 +0700

    done truyenvuadang

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import SlideNoiBat from './components/SlideNoiBat';
 import LichSuDoc from './components/LichSuDoc';
 import TruyenMoi from './components/TruyenMoi';
 import ChuongMoi from './components/ChuongMoi';
+import TruyenVuaDang from './components/TruyenVuaDang';
 
 export default function Home() {
 	return (
@@ -27,6 +28,18 @@ export default function Home() {
 				<div className='flex flex-col justify-center gap-2 lg:flex-row'>
 					<div className='order-2 mx-auto w-10/12 lg:order-1 lg:m-0 lg:w-6/12'>
 						<ChuongMoi />
+					</div>
+					<div className='order-1 mx-auto w-10/12	lg:order-2 lg:m-0 lg:w-4/12'>
+						{/* Component theo dõi nhiều để ở đây */}
+					</div>
+				</div>
+
+				<div className='flex flex-col justify-center gap-2 lg:flex-row'>
+					<div className='order-2 mx-auto w-10/12 lg:order-1 lg:m-0 lg:w-6/12'>
+						{/* component swiper truyện đã hoàn thành để ở đây */}
+					</div>
+					<div className='order-1 mx-auto w-10/12	lg:order-2 lg:m-0 lg:w-4/12'>
+						<TruyenVuaDang />
 					</div>
 				</div>
 			</div>
