f7457eebd61ce7e2377dbc83fabb4fb855bb1819
manden520 <92621988+manden520@users.noreply.github.com>
2023-08-17 22:07:49 +0700

    up dashboard

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,12 +1,16 @@
 'use client';
+import Dashboard from './components/Dashboard';
 import Sidebar from './components/Sidebar';
 export default function AdminMain() {
 	return (
-		<div>
+		<div className='flex'>
 			<div className='flex flex-col lg:flex-row'>
 				<Sidebar />
 			</div>
-			<h3>Trang quản trị</h3>
+
+			<div className='flex flex-grow flex-col'>
+				<Dashboard />
+			</div>
 		</div>
 	);
 }
