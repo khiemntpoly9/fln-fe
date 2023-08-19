f7cc00d3f71c3fe8043a322c838fc304a9ca9d53
Nguyễn Trung Khiêm <trungkhiem1412@gmail.com>
2023-08-19 15:05:10 +0700

    fix

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,7 +1,15 @@
 'use client';
+import { useContext } from 'react';
+import { useRouter } from 'next/navigation';
+import { Context } from '@/contexts/context';
 import Sidebar from '@/admin/components/Sidebar';
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
+	const router = useRouter();
+	const { user } = useContext(Context);
+	if (!user.isLogin || user.role !== 'admin') {
+		return router.push('/');
+	}
 	return (
 		<div className='flex'>
 			<div className='flex flex-col lg:flex-row'>
