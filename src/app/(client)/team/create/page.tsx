97f87968ba680a1b3ccaf68cdf5b6553bef7bedc
Nguyễn Trung Khiêm <trungkhiem1412@gmail.com>
2023-08-23 19:14:18 +0700

    validate tạo nhóm dịch

--- src/app/(client)/team/create/page.tsx
+++ src/app/(client)/team/create/page.tsx
@@ -1,11 +1,25 @@
 'use client';
 import { createTeam } from '@/services/team.service';
 import { useMutation } from '@tanstack/react-query';
+import { useState } from 'react';
 export default function TeamCreate() {
+	const [isShowError, setIsShowError] = useState<boolean>(false);
+	const [isMessageError, setIsMessageError] = useState<string>('');
 	// api create team
 	const { mutate } = useMutation({
 		mutationFn: (data: { team_name: string; team_detail: string }) => createTeam(data),
 	});
+	// Validate
+	const validate = (data: { team_name: string; team_detail: string }) => {
+		if (!data.team_name && data.team_name === '') {
+			setIsMessageError('Vui lòng nhập tên nhóm dịch!');
+			return false;
+		} else if (data.team_name.length < 5) {
+			setIsMessageError('Têm nhóm phải hơn 5 ký tự!');
+			return false;
+		}
+		return true;
+	};
 	// Submit
 	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
@@ -15,9 +29,13 @@ export default function TeamCreate() {
 			team_detail: target.description.value,
 		};
 		// validate
-
-		// call api
-		mutate(data_form);
+		if (validate(data_form)) {
+			setIsShowError(false);
+			// call api
+			mutate(data_form);
+		} else {
+			setIsShowError(true);
+		}
 	};
 	return (
 		<div className='container mx-auto w-10/12'>
@@ -27,10 +45,13 @@ export default function TeamCreate() {
 					<div className='mb-4 flex flex-col'>
 						<label className='mb-2'>Tên nhóm dịch</label>
 						<input
-							className='appearance-none rounded border p-2 shadow focus:outline-none'
+							className={`${
+								isShowError ? 'mb-2 border-rose-600' : ''
+							} appearance-none rounded border p-2 shadow focus:outline-none`}
 							type='text'
 							id='nameteam'
 						/>
+						{isShowError && <p className='text-sm italic text-red-500'>{isMessageError}</p>}
 					</div>
 					<div className='mb-4 flex flex-col'>
 						<label className='mb-2'>Mô tả nhóm</label>
